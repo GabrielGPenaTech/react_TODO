d046717132445f3d49bae267bd5fd88d3da60c17
GabrielGPena <kbeca.gabriel@hotmail.com>
2025-09-21 15:39:35 -0300

    feat: refactor App component and add Text and Icon components
    - Updated App component to use new Text component for rendering text with different styles.
    - Introduced Icon component to handle SVG icons with animation support.
    - Added class variance authority for styling variants in both Text and Icon components.
    - Integrated SVGR plugin for handling SVG imports in the project.
    - Updated main.tsx to import App as a named export.
    - Added type definitions for SVGR in vite-env.d.ts.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-function App() {
+import { Text } from "./components/text";
+import SpinIcon from "./assets/icons/spinner.svg?react"
+import { Icon } from "./components/icon";
+
+export function App() {
   return (
-    <>
-      <h1 className="text-pink-dark">Olá zé</h1>
-    </>
+    <div className="grid g-3">
+      <div className="flex flex-col gap-2">
+        <Text variant="body-sm-bold" as="h1" className="text-pink-dark">Olá zé</Text>
+        <Text variant="body-md-bold" as="h1" className="text-pink-base">Olá zé</Text>
+        <Text>Olá zé</Text>
+      </div>
+
+      <div className="flex flex-col">
+        <Icon svg={SpinIcon} animate className="fill-green-base" />
+      </div>
+    </div>
   )
 }
-
-export default App
